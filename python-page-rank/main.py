c98f2c4e9b852b2287a45489c62f3229e1b7bb35
Cabbage Stone <calebstone800@gmail.com>
2024-02-13 18:35:56 -0700

    add initial search functionality

--- python-page-rank/main.py
+++ python-page-rank/main.py
@@ -1,26 +1,37 @@
 import os
+import sys
 from dotenv import load_dotenv
-import redis
+from redis import Redis
+from wikifetcher import WikiFetcher
+from pagetools import redis_index_pipeline, link_generator
 
 def main():
-  # Load environment variables from .env file
   load_dotenv()
 
-  # Get Redis connection details from environment variables
   redis_host = os.getenv("REDIS_HOST")
   redis_port = os.getenv("REDIS_PORT")
-  redis_password = os.getenv("REDIS_PASSWORD")
 
-  # Connect to Redis
-  r = redis.Redis(host=redis_host, port=redis_port, password=redis_password)
+  redis_client = Redis(host=redis_host, port=redis_port)
 
-  # Test the connection
   try:
-    r.ping()
+    redis_client.ping()
     print("Connected to Redis successfully!")
-  except redis.ConnectionError:
-    print("Failed to connect to Redis.")
+  except Exception as e:
+    print("Failed to connect to Redis." + str(e))
     return
+  
+  fetcher = WikiFetcher()
+
+  searchTerm = sys.argv[1]
+  searchURL = "https://en.wikipedia.org/wiki/Special:Search?go=Go&search="+searchTerm+"&ns0=1"
+
+  page = fetcher.fetch_wikipedia(searchURL)
+
+  redis_index_pipeline(page, searchURL, redis_client)
+
+  for pageURL in link_generator(page):
+    print("https://en.wikipedia.org" + pageURL.get('href'))
+
 
 if __name__ == "__main__":
   main()
