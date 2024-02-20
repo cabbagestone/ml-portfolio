b8cfe9d683f2986b7f0693f5f03f066212d53c76
Cabbage Stone <calebstone800@gmail.com>
2024-02-19 19:00:52 -0700

    some psuedo code for calculating page ranking for search

--- python-page-rank/datatools.py
+++ python-page-rank/datatools.py
@@ -1,4 +1,6 @@
 from collections import deque
+from pagerank import pagerank_numpy
+import networkx as nx
 
 def reachable_nodes_bfs(G, start):
     seen = set()
@@ -10,3 +12,19 @@ def reachable_nodes_bfs(G, start):
             neighbors = set(G[node]) - seen
             queue.extend(neighbors)
     return seen
+
+def calculate_page_rank(link_graph):
+    G = nx.from_dict_of_lists(link_graph) 
+    return pagerank_numpy(G)
+
+def calculate_hybrid_score(page_url, page_rank, redis_client):
+    index_score = calculate_index_score(page_url, redis_client) 
+    
+    normalized_page_rank = page_rank / max(page_rank.values()) 
+    normalized_index_score = index_score / ...
+
+    hybrid_score = 0.7 * normalized_page_rank + 0.3 * normalized_index_score  
+    return hybrid_score
+
+def calculate_index_score(page_url, redis_client):
+    return 0.5
